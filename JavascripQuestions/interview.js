73e5f491a7b60d5e261e6a5ffcc3d155cad721e9
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-14 14:54:37 +0200

    new int

--- JavascripQuestions/interview.js
+++ JavascripQuestions/interview.js
@@ -70,3 +70,59 @@ function uniqueArray(array) {
   return unique;
 }
 
+// > var x; // declaring x
+// > console.log(x); //output: undefined
+
+// var k = 1;
+// if (1) {
+//   function foo(){};
+//   k += typeof foo;
+// }
+// console.log(k); // output 1function
+
+
+
+var Employee = function (name, company, salary) {
+  this.name = name || "";       //Public attribute default value is null
+  this.company = company || ""; //Public attribute default value is null
+  this.salary = salary || 5000; //Public attribute default value is null
+
+  // Private method
+  var increaseSalary = function () {
+      this.salary = this.salary + 1000;
+  };
+
+  // Public method
+  this.dispalyIncreasedSalary = function() {
+      increaseSlary();
+      console.log(this.salary);
+  };
+};
+
+// Create Employee class object
+var emp1 = new Employee("John","Pluto",3000);
+// Create Employee class object
+var emp2 = new Employee("Merry","Pluto",2000);
+// Create Employee class object
+var emp3 = new Employee("Ren","Pluto",2500);
+
+
+var globalVar = "abc";
+
+// Parent self invoking function
+(function outerFunction (outerArg) { // begin of scope outerFunction
+    // Variable declared in outerFunction function scope
+    var outerFuncVar = 'x';
+    // Closure self-invoking function
+    (function innerFunction (innerArg) { // begin of scope innerFunction
+        // variable declared in innerFunction function scope
+        var innerFuncVar = "y";
+        console.log(
+            "outerArg = " + outerArg + "\n" +
+            "outerFuncVar = " + outerFuncVar + "\n" +
+            "innerArg = " + innerArg + "\n" +
+            "innerFuncVar = " + innerFuncVar + "\n" +
+            "globalVar = " + globalVar);
+
+    }// end of scope innerFunction)(5); // Pass 5 as parameter
+}// end of scope outerFunction )(7); // Pass 7 as parameter
