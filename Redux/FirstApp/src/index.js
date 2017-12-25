cb3fab11d5a7708d864dd08dbeecdb6ab4406798
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-25 16:39:01 +0200

    finished -+

--- Redux/FirstApp/src/index.js
+++ Redux/FirstApp/src/index.js
@@ -1,36 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux';
+
+const initialState = { count: 0 };
+
+let reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.amount };
+    case 'DECREMENT':
+      return { count: state.count - action.amount };
+    case 'RESET':
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
+
+// const incrementAction = { type: 'INCREMENT', amount: 1 };
+// const decrementAction = { type: 'DECREMENT', amount: 1 };
+// const resetAction = { type: 'RESET', amount: initialState.count };
+
+let increment = amount => {
+  return { type: 'INCREMENT', amount };
+};
+
+let decrement = amount => {
+  return { type: 'DECREMENT', amount };
+};
+
+let reset = () => {
+  return { type: 'RESET' };
+};
+
+const store = createStore(reducer, initialState);
+
 class Counter extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { count: 0 };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
 
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-    }
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
+  }
 
-    increment() {
-        this.setState({ count: this.state.count + 1 });
-    }
+  increment() {
+    let amount = parseInt(this.refs.amount.value || 1);
+    store.dispatch(increment(amount));
+  }
 
-    decrement() {
-        this.setState({ count: this.state.count - 1 });
-    }
+  decrement() {
+    let amount = parseInt(this.refs.amount.value || 1);
+    store.dispatch(decrement(amount));
+  }
 
-    render() {
-        return (
-            <div className="counter">
-                <span className="count">{this.state.count}</span>
+  reset() {
+    store.dispatch(reset());
+  }
 
-                <div className="buttons">
-                    <button className="decrement" onClick={this.decrement}>-</button>
-                    <button className="increment" onClick={this.increment}>+</button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const count = store.getState().count;
+    return (
+      <div className="counter">
+        <span className="count">{count}</span>
+
+        <div className="buttons">
+          <button className="decrement" onClick={this.decrement}>
+            -
+          </button>
+          <button className="reset" onClick={this.reset}>
+            delete
+          </button>
+          <button className="increment" onClick={this.increment}>
+            +
+          </button>
+        </div>
+        <input type="text" ref="amount" defaultValue="1" />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
