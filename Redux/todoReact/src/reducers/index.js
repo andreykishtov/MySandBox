c959f35274f5d75e1c5ab6c3070af6bb9e2eaaef
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-27 09:44:56 +0200

    finished reducers

--- Redux/todoReact/src/reducers/index.js
+++ Redux/todoReact/src/reducers/index.js
@@ -1,43 +1,48 @@
 import { ADD_TODO, TOGGLE_TODO, EDIT_TODO, DELETE_TODO } from '../actions/index';
 
-function todoReducer(params) {}
-
-export default function reducer(state = [], action) {
+function todoReducer(state = {}, action) {
   switch (action.type) {
     case ADD_TODO:
-      const todo = {
+      return {
         id: action.id,
         title: action.title,
         completed: false
       };
+    case TOGGLE_TODO:
+      if (state.id !== action.id) {
+        return state;
+      }
 
-      return [...state, todo];
+      return Object.assign({}, state, {
+        completed: !state.completed
+      });
+    case EDIT_TODO:
+      if (state.id !== action.id) {
+        return state;
+      }
+
+      return Object.assign({}, state, {
+        title: action.title
+      });
+
+    default:
+  }
+}
+
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, todoReducer(undefined, action)];
 
     case DELETE_TODO:
       const index = state.findIndex(todo => todo.id === action.id);
       return [...state.slice(0, index), ...state.slice(index + 1)];
 
     case TOGGLE_TODO:
-      return state.map(todo => {
-        if (todo.id !== action.id) {
-          return todo;
-        }
-
-        return Object.assign({}, todo, {
-          completed: !todo.completed
-        });
-      });
+      return state.map(todo => todoReducer(todo, action));
 
     case EDIT_TODO:
-      return state.map(todo => {
-        if (todo.id !== action.id) {
-          return todo;
-        }
-
-        return Object.assign({}, todo, {
-          title: action.title
-        });
-      });
+      return state.map(todo => todoReducer(todo, action));
 
     default:
       return state;
