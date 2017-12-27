72885afd886554881e60929fa580fa9daa4f67df
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-27 15:10:57 +0200

    filter works

--- Redux/FilterLink/src/containers/ListContainer.js
+++ Redux/FilterLink/src/containers/ListContainer.js
@@ -3,18 +3,31 @@ import { connect } from 'react-redux';
 import { deleteTodo, toggleTodo, editTodo } from '../actions';
 import List from '../components/List';
 
+function getFilteredTodos(todos, filter) {
+  switch (filter) {
+    case 'ALL':
+      return todos;
+    case 'COMPLETED':
+      return todos.filter(todo => todo.completed);
+    case 'UNCOMPLETED':
+      return todos.filter(todo => !todo.completed);
+    default:
+      break;
+  }
+}
+
 function mapStateToProps(state) {
-    return {
-        todos: state
-    };
+  return {
+    todos: getFilteredTodos(state.todos, state.filter)
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        onDelete: id => dispatch(deleteTodo(id)),
-        onToggle: id => dispatch(toggleTodo(id)),
-        onEdit: (id, title) => dispatch(editTodo(id, title))
-    };
+  return {
+    onDelete: id => dispatch(deleteTodo(id)),
+    onToggle: id => dispatch(toggleTodo(id)),
+    onEdit: (id, title) => dispatch(editTodo(id, title))
+  };
 }
 
 const ListContainer = connect(mapStateToProps, mapDispatchToProps)(List);
