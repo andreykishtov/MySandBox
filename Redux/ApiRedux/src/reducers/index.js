d27e029a2e8cf79c5297decfcaad49a7d78fc452
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-27 16:21:20 +0200

    redux thunk

--- Redux/ApiRedux/src/reducers/index.js
+++ Redux/ApiRedux/src/reducers/index.js
@@ -2,14 +2,16 @@ import { combineReducers } from 'redux';
 
 import todos, * as fromTodos from './todo';
 import filter from './filter';
+import fetching from './fetching';
 
 const reducer = combineReducers({
-    todos,
-    filter
+  todos,
+  filter,
+  fetching
 });
 
 export default reducer;
 
 export function getFilteredTodos(state) {
-    return fromTodos.getFilteredTodos(state.todos, state.filter);
+  return fromTodos.getFilteredTodos(state.todos, state.filter);
 }
