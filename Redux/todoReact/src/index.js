0a178c95c9f2cc0fb246cdd8eb56d26598a0460c
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-27 10:22:18 +0200

    checking store before adding to app

--- Redux/todoReact/src/index.js
+++ Redux/todoReact/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
+import reducer from './reducers';
 import todos from './todos';
 import App from './App';
+import { addTodo, deleteTodo, editTodo, toggleTodo } from './actions';
+
+const store = createStore(reducer, todos);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addTodo('check stuff'));
+store.dispatch(toggleTodo(5));
+store.dispatch(editTodo(5, 'delete stuff'));
+store.dispatch(deleteTodo(5));
 
 ReactDOM.render(<App initialData={todos} />, document.getElementById('root'));
