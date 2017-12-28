4cb311bd06237b3a3f68fc2dbf3bd69402b754e9
Andrey Kishtov <andreykishtov@gmail.com>
2017-12-28 11:51:01 +0200

    book creaton

--- book-list-app/client/src/components/App.jsx
+++ book-list-app/client/src/components/App.jsx
@@ -4,27 +4,42 @@ import styled from 'styled-components';
 const api = 'https://www.googleapis.com/books/v1/volumes?q=harry+potter';
 
 const Wrapper = styled.div`
-  width: 100px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 10px;
+`;
+
+const Book = styled.div`
+  border: 1px solid grey;
 `;
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: 'loading' };
+    this.state = { items: [] };
   }
 
   componentWillMount = async () => {
     const response = await fetch(api);
     const json = await response.json();
-    console.log(json);
-    this.setState({ data: json });
+    this.setState({ items: json.items });
   };
 
   render() {
+    const { items } = this.state;
+    console.log(items);
     return (
-      <Wrapper>
-        <p>{this.state.data}</p>
-      </Wrapper>
+      items.length && (
+        <Wrapper>
+          {items.map(book => (
+            <Book key={book.id}>
+              <h1>{book.volumeInfo.title}</h1>
+              <p>{book.volumeInfo.publishedDate}</p>
+              <p>{book.volumeInfo.authors}</p>
+            </Book>
+          ))}
+        </Wrapper>
+      )
     );
   }
 }
