588b88323ab5df4cc0ff2512d1e001ee9b2ef8ba
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-01 23:51:26 +0200

    styled books

--- book-list-app/client/src/components/Books.jsx
+++ book-list-app/client/src/components/Books.jsx
@@ -1,48 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Buttons from './Buttons';
-
-const Wrapper = styled.div`
-  width: 200px;
-  display: grid;
-  padding-bottom: 10px;
-  background: #27282a;
-  justify-items: center;
-  grid-template-areas: 'Image Image' 'Title Title' ' Author Author' 'PublishDate PublishDate' 'Buttons Buttons';
-`;
-
-const Title = styled.h2`
-  margin-bottom: 5px;
-  grid-area: Title;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  max-width: 180px;
-  overflow: hidden;
-  color: white;
-`;
-
-const PublishDate = styled.p`
-  color: #797a7c;
-  margin: 0;
-  align-self: center;
-  grid-area: PublishDate;
-`;
-
-const Author = styled.p`
-  color: #797a7c;
-  margin: 0 0 15px 0;
-  grid-area: Author;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  max-width: 180px;
-  overflow: hidden;
-`;
-
-const Image = styled.img`
-  width: 200px;
-  grid-area: Image;
-  justify-self: center;
-`;
+import { Wrapper, Title, PublishDate, Author, Image } from './Books.styled.js';
 
 const Book = ({ book, onChangeBook, onDelete }) => (
   <Wrapper key={book.id}>
