545b61c55f03dcb3ef75cad21f0cdb4b849ebc8f
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-05 21:29:25 +0200

    modal redone

--- book-list-app/client/src/components/Modal.jsx
+++ book-list-app/client/src/components/Modal.jsx
@@ -1,24 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  border: 1px solid #4f5262;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 150;
-`;
-
-const BackDrop = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0px;
-  left: 0px;
-  z-index: 100;
-  background: rgba(0, 0, 0, 0.3);
-`;
+import { Wrapper, BackDrop } from '../styled/Modal.styled';
 
 const Modal = ({ onClose, children, isOpen }) =>
   isOpen && (
---
+++ book-list-app/client/src/styled/Modal.styled.js
@@ -0,0 +1,18 @@
+export const Wrapper = styled.div`
+  border: 1px solid #4f5262;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 150;
+`;
+
+export const BackDrop = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.3);
+`;
