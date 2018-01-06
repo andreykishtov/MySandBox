6298a257d879eeab8d19483df30dda11d501a522
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-06 22:50:40 +0200

    main reducer

--- book-list-app/client/src/reducers/index.js
+++ book-list-app/client/src/reducers/index.js
@@ -1,19 +1,9 @@
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(todo => (todo.id === action.id ? { ...todo, completed: !todo.completed } : todo));
-    default:
-      return state;
-  }
-};
+import { combineReducers } from 'redux';
+//import todos from './todos'
+//import visibilityFilter from './visibilityFilter'
 
-export default todos;
+const todoApp = combineReducers({
+  //reducers
+});
+
+export default todoApp;
