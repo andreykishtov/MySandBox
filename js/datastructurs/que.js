1f491696d8a4b829b3cd9d844036c451089f32df
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-08 10:52:07 +0200

    finishe queue v0

--- js/datastructurs/que.js
+++ js/datastructurs/que.js
@@ -1,7 +1,42 @@
-const que = (() => {
-  const queObj = {};
+const que = (queueSize => {
+  const array = [];
+  let countInsert = 0;
+  let head = 0;
+  let tail = 0;
 
-  queObj.createQue = () => {};
+  array.length = queueSize;
 
-  return;
+  const addToQueue = data => {
+    if (countInsert === array.length) {
+      return console.log('error no more space');
+    }
+    array[tail] = data;
+    tail += 1;
+    countInsert++;
+    return console.log(`inserted: ${data} to queue`);
+  };
+
+  const getHead = () => {
+    if (!countInsert) {
+      return console.log('error: array is empty');
+    }
+    countInsert--;
+    head += 1;
+    return console.log(`your data: ${array[head - 1]}`);
+  };
+
+  return { addToQueue, getHead };
 })();
+
+const queue = que(5);
+
+queue.addToQueue('1');
+queue.addToQueue('2');
+queue.addToQueue('3');
+queue.addToQueue('4');
+
+queue.getHead();
+queue.getHead();
+queue.getHead();
+queue.getHead();
+queue.getHead();
