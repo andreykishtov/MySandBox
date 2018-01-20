ee6294e7f49c7f107de9c5e36d52f2a68e326196
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-20 13:18:39 +0200

    piramid recursion

--- IntrerviewQuestions/pyramid/index.js
+++ IntrerviewQuestions/pyramid/index.js
@@ -14,34 +14,33 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  for (let row = 0; row < n; row++) {
-    let level = '';
-
-    for (let column = 0; column < 2 * n - 1; column++) {
-      const midPoint = Math.floor((2 * n - 1) / 2);
-      column >= midPoint - row && column <= midPoint + row ? (level += '#') : (level += ' ');
-    }
-    console.log(level);
+function pyramid(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
   }
+
+  if (2 * n - 1 === stair.length) {
+    console.log(`${stair}`);
+    return pyramid(n, row + 1);
+  }
+
+  let middle = Math.floor((2 * n - 1) / 2);
+
+  stair.length >= middle - row && stair.length <= middle + row ? (stair += '#') : (stair += ' ');
+
+  pyramid(n, row, stair);
 }
 
 module.exports = pyramid;
 
-// function pyramid(n, row = 0, stair = '') {
-//   if (n === row) {
-//     return;
+// function pyramid(n) {
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       const midPoint = Math.floor((2 * n - 1) / 2);
+//       column >= midPoint - row && column <= midPoint + row ? (level += '#') : (level += ' ');
+//     }
+//     console.log(level);
 //   }
-
-//   if (n + n - 1 === stair.length) {
-//     console.log(`|${stair}|`);
-//     return pyramid(n, row + 1);
-//   }
-
-//   let middle = n % 2 === 0 || n === 1 ? Math.floor(n / 2) : Math.ceil(n / 2);
-
-//   stair.length >= middle - row && stair.length <= middle + row ? (stair += '#') : (stair += ' ');
-
-//   pyramid(n, row, stair);
 // }
-// pyramid(4);
