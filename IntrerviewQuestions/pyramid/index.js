79d104a11bec9a6c33467f076bd3c41d9716f4e1
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-20 13:15:22 +0200

    piramid

--- IntrerviewQuestions/pyramid/index.js
+++ IntrerviewQuestions/pyramid/index.js
@@ -15,8 +15,33 @@
 //       '#####'
 
 function pyramid(n) {
-  const row = n;
-  const column = n === 1 ? n : n + 2;
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      const midPoint = Math.floor((2 * n - 1) / 2);
+      column >= midPoint - row && column <= midPoint + row ? (level += '#') : (level += ' ');
+    }
+    console.log(level);
+  }
 }
 
 module.exports = pyramid;
+
+// function pyramid(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return;
+//   }
+
+//   if (n + n - 1 === stair.length) {
+//     console.log(`|${stair}|`);
+//     return pyramid(n, row + 1);
+//   }
+
+//   let middle = n % 2 === 0 || n === 1 ? Math.floor(n / 2) : Math.ceil(n / 2);
+
+//   stair.length >= middle - row && stair.length <= middle + row ? (stair += '#') : (stair += ' ');
+
+//   pyramid(n, row, stair);
+// }
+// pyramid(4);
