c5710e41fe35cb932855d179252141fbf12a1822
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-22 13:26:27 +0200

    fib problem with hof memorize function

--- IntrerviewQuestions/fib/index.js
+++ IntrerviewQuestions/fib/index.js
@@ -8,17 +8,47 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-  const arr = [0, 1];
+function memorize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
 
-  let index = 2;
-
-  while (index <= n) {
-    arr.push(arr[index - 1] + arr[index - 2]);
-    index++;
-  }
-
-  return arr[n];
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
 }
 
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memorize(fib);
+
 module.exports = fib;
+
+// function fib(n, last = 0, curr = 1) {
+//   if (n === 0) {
+//     return last;
+//   }
+//   return fib(n - 1, curr, last + curr);
+// }
+
+// function fib(n) {
+//   const arr = [0, 1];
+
+//   let index = 2;
+
+//   while (index <= n) {
+//     arr.push(arr[index - 1] + arr[index - 2]);
+//     index++;
+//   }
+
+//   return arr[n];
+// }
