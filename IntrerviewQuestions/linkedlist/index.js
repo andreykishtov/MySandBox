db4ef45636b96c0d893e921dbc193ba236c3ba35
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-22 18:15:33 +0200

    linked list constructor node costructor and insertfirst

--- IntrerviewQuestions/linkedlist/index.js
+++ IntrerviewQuestions/linkedlist/index.js
@@ -2,8 +2,21 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(record) {
+    this.head = new Node(record, this.head);
+  }
+}
 
 module.exports = { Node, LinkedList };
