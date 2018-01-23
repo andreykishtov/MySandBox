e600bebb846ec8018fcc12d64022d9e390718c2d
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-23 12:25:10 +0200

    linked list

--- IntrerviewQuestions/linkedlist/index.js
+++ IntrerviewQuestions/linkedlist/index.js
@@ -17,6 +17,58 @@ class LinkedList {
   insertFirst(record) {
     this.head = new Node(record, this.head);
   }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      node = node.next;
+      counter++;
+    }
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let last = this.head;
+    while (last.next !== null) {
+      last = last.next;
+    }
+    return last;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    let curr = this.head;
+    let prev = curr;
+
+    if (curr === null) {
+      return null;
+    }
+
+    if (curr.next === null) {
+      this.head = null;
+      return curr;
+    }
+
+    while (curr.next) {
+      prev = curr;
+      curr = curr.next;
+    }
+
+    prev.next = null;
+    return curr;
+  }
 }
 
 module.exports = { Node, LinkedList };
