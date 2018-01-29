186b2eda9e8d6e9f85e77de36a4479f6c973d9d4
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-29 17:21:10 +0200

    start navbar

--- IntrerviewQuestions/bst/index.js
+++ IntrerviewQuestions/bst/index.js
@@ -11,6 +11,40 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+    if (this.data === data) return this;
+    if (!this.data) return null;
+    return this.data > data ? this.left.contains(data) : this.right.contains(data);
+  }
+}
 
 module.exports = Node;
+
+// insert(data, node = this) {
+//   if (data < node.data) {
+//     if (!node.left) return (node.left = new Node(data));
+//     this.insert(data, node.left);
+//   } else {
+//     if (!node.right) return (node.right = new Node(data));
+//     this.insert(data, node.right);
+//   }
+// }
