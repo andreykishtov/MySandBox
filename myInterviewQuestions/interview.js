b2fddcf1d50ce46d245ed8fad106c518153cc4a5
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-31 17:47:24 +0200

    maxMultiple

---
+++ myInterviewQuestions/interview.js
@@ -0,0 +1,21 @@
+// create classes with different abilities. space invaders
+
+class Monsters {
+  constructor(health) {
+    this.health = health;
+  }
+}
+
+class round extends Monsters {
+  constructor(params) {
+    super(params);
+  }
+}
+
+class circle extends Monsters {
+  constructor(params) {
+    super(params);
+  }
+}
+
+// create sql querry.
