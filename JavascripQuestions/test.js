ac879d915a69176f68712a27f93d1ddc9c08a5be
Andrey Kishtov <andreykishtov@gmail.com>
2018-02-07 23:43:48 +0200

    test

--- JavascripQuestions/test.js
+++ JavascripQuestions/test.js
@@ -23,3 +23,56 @@ function check(Arr) {
   }, -Infinity);
   return result;
 }
+
+$('.main').mouseup(function() {
+  var range, sel;
+  var colour = 'yellow';
+
+  if (window.getSelection) {
+    // IE9 and non-IE
+    try {
+      if (!document.execCommand('BackColor', false, colour)) {
+        makeEditableAndHighlight();
+      }
+    } catch (ex) {
+      makeEditableAndHighlight();
+    }
+  }
+});
+
+function makeEditableAndHighlight() {
+  var colour = 'yellow';
+  var range,
+    sel = window.getSelection();
+  if (sel.rangeCount && sel.getRangeAt) {
+    range = sel.getRangeAt(0);
+  }
+  document.designMode = 'on';
+  if (range) {
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+  // Use HiliteColor since some browsers apply BackColor to the whole block
+  if (!document.execCommand('HiliteColor', false, colour)) {
+    document.execCommand('BackColor', false, colour);
+  }
+  document.designMode = 'off';
+
+  saveSelections(sel);
+}
+
+function saveSelections(selection) {
+  if (selection) {
+    var highlight = selection.toString();
+    // localStorage.setItem('textSelection', highlight);
+  }
+}
+
+function restoreSelection() {
+  //var highlight = localStorage.getItem('textSelection');
+
+  if (highlight) {
+    //I am stumped here I have tried
+    //CreateRange() but nothing works
+  }
+}
